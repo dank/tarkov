2fb39d737a5cc76a40bd5f620ad2039b2af7fa32
Dan Kyung <dan@kyung.ca>
2020-01-12 23:27:24 -0500

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+const LAUNCHER_ENDPOINT: &str = "https://launcher.escapefromtarkov.com";
+const PROD_ENDPOINT: &str = "https://prod.escapefromtarkov.com";
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
