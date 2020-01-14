a92f1e35aef88f35adc62ccd991b8aee61ab3f6e
Dan Kyung <dan@kyung.ca>
2020-01-13 20:36:48 -0500

    Login!

--- examples/auth.rs
+++ examples/auth.rs
@@ -1,7 +1,9 @@
 use tarkov::Tarkov;
 
 // TODO: Change my password soon!
-fn main() {
+
+#[actix_rt::main]
+async fn main() {
     let t = Tarkov::new();
-    t.login("***REMOVED***", "***REMOVED***");
+    t.login("***REMOVED***", "***REMOVED***", "#1-17620e25e76ab3031bd59ee937a463b289cb5698:a0c1ec9e624f0e6732e19221d0cf7cd75f6fb570:9558bf53513b1dc06f7f2fd78883cd0326f00e42-55e722c489976f9349796e35f87594d3569872d9-5f929b6d87f35ef70f3563d4e02e7ff438f67c3a-8c60765c988e992f0c858626407fd97fe2392799-75fd29e7ea40550d9bab6bf0fea1fd9cc8cf53e1-e1bc65a216325f0ad0db8518fa299db2").await;
 }
--- src/auth.rs
+++ src/auth.rs
@@ -1,24 +1,43 @@
-use crate::{Tarkov, LAUNCHER_ENDPOINT, LAUNCHER_VERSION};
+use crate::{Tarkov, LAUNCHER_ENDPOINT, LAUNCHER_VERSION, Result};
 use actix_web::http::StatusCode;
 use serde::Serialize;
+use std::str::from_utf8;
+use std::io::Read;
+use flate2::read::ZlibDecoder;
 
 #[derive(Debug, Serialize)]
-struct LoginRequest<'a>{
+#[serde(rename_all = "camelCase")]
+struct LoginRequest<'a> {
     email: &'a str,
-    password: &'a str,
+    pass: &'a str,
+    hw_code: &'a str,
+    captcha: Option<()>, // Always null
 }
 
 impl Tarkov {
-    pub async fn login(&self, email: &str, password: &str) {
+    pub async fn login(&self, email: &str, password: &str, hw_code: &str) -> Result<()> {
         let url = format!("{}/launcher/login?launcherVersion={}&branch=live", LAUNCHER_ENDPOINT, LAUNCHER_VERSION);
+        let password = format!("{:x}", md5::compute(&password));
         let req = LoginRequest {
             email,
-            password: "a",
+            pass: &password,
+            hw_code,
+            captcha: None
         };
-        let mut res = self.client.post(url).send_json(&req).await.unwrap();
-        let body = res.body().await.unwrap();
 
-        println!("{:?}", body);
+        let mut res = self.client.post(url)
+            .header("User-Agent", "UnityPlayer/5.6.5p3")
+            .send_json(&req)
+            .await?;
+        let body = res.body().await?;
+        let mut decode = ZlibDecoder::new(&body[..]);
+        let mut body = String::new();
+        decode.read_to_string(&mut body)?;
+
+        println!("{}", body);
+
+        Ok(())
+
 //        match res.status() {
 //            StatusCode::OK => Ok(serde_json::from_slice::<T>(&body)?),
 //            _ => {}
