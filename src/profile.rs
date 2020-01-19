d9d7e9c9b1b8c60552a53f5d7e382c9c5c9bf47b
Dan Kyung <dan@kyung.ca>
2020-01-18 21:32:12 -0500

    Profile docs

--- src/profile.rs
+++ src/profile.rs
@@ -17,325 +17,448 @@ struct ProfileResponse {
     data: Option<Vec<Profile>>,
 }
 
+/// Profile
 #[derive(Debug, Deserialize, Clone, PartialEq)]
 pub struct Profile {
+    /// Profile ID
     #[serde(rename = "_id")]
     pub id: String,
+    /// ?
     pub aid: u64,
+    /// SCAV profile ID
     pub savage: Option<String>,
+    /// Profile info
     #[serde(rename = "Info")]
     pub info: Info,
+    /// Character customization
     #[serde(rename = "Customization")]
     pub customization: Customization,
+    /// Character health
     #[serde(rename = "Health")]
     pub health: Health,
+    /// Inventory
     #[serde(rename = "Inventory")]
     pub inventory: Inventory,
+    /// Skills
     #[serde(rename = "Skills")]
     pub skills: Skills,
+    /// Stats
     #[serde(rename = "Stats")]
     pub stats: Stats,
+    /// Encyclopedia
     #[serde(rename = "Encyclopedia")]
     pub encyclopedia: HashMap<String, bool>,
+    /// ?
     #[serde(rename = "ConditionCounters")]
     pub condition_counters: ConditionCounters,
+    /// ?
     #[serde(rename = "BackendCounters")]
     pub backend_counters: HashMap<String, BackendCounter>,
+    // XXX: Find types for these
     // insured_items: [],
     // hideout: {},
     // notes: {},
+    /// Bonuses?
     #[serde(rename = "Bonuses")]
     pub bonuses: Vec<Bonus>,
+    /// Active quests
     #[serde(rename = "Quests")]
     pub quests: Vec<Quest>,
+    /// Flea market stats
     #[serde(rename = "RagfairInfo")]
     pub ragfair: Ragfair,
+    // XXX: Find types for these
     // trader_standings: [],
     // wish_list: [],
 }
 
+/// Profile info
 #[derive(Debug, Deserialize, Clone, PartialEq)]
 #[serde(rename_all = "PascalCase")]
 pub struct Info {
+    /// Profile nickname
     pub nickname: String,
+    /// Profile lowercase nickname
     pub lower_nickname: String,
+    /// Profile side
     pub side: Side,
+    /// Profile voice
     pub voice: String,
+    /// Profile level
     pub level: u64,
+    /// Profile experience points
     pub experience: u64,
+    /// Profile registration timestamp
     pub registration_date: u64,
+    /// Game version
     pub game_version: String,
+    /// Profile type
     pub account_type: u64,
     // XXX: Bad devs! This field can be both String and integer, ignoring for now.
     // member_category: String,
+    /// ?
     #[serde(rename = "lockedMoveCommands")]
     pub locked_move_commands: bool,
+    /// SCAV cooldown timestamp
     pub savage_lock_time: u64,
+    /// Last time played as SCAV
     pub last_time_played_as_savage: u64,
+    /// Profile settings
     pub settings: Settings,
+    /// ?
     pub need_wipe: bool,
+    /// ?
     pub global_wipe: bool,
+    /// Profile nickname change timestamp
     pub nickname_change_date: u64,
-    // bans: []
+    // XXX: find types
+    //    pub bans: []
 }
 
+/// Faction, team or side.
 #[derive(Debug, Deserialize, Clone, PartialEq)]
 pub enum Side {
+    /// BEAR faction
     Bear,
+    /// USEC faction
     Usec,
+    /// SCAV
     Savage,
 }
 
+/// Profile settings
 #[derive(Debug, Deserialize, Clone, PartialEq)]
 #[serde(rename_all = "PascalCase")]
 pub struct Settings {
+    /// ?
     pub role: Option<String>,
+    /// ?
     pub bot_difficulty: Option<String>,
+    /// ?
     pub experience: Option<i64>,
 }
 
+/// Player customization
 #[derive(Debug, Deserialize, Clone, PartialEq)]
 #[serde(rename_all = "PascalCase")]
 pub struct Customization {
+    /// Head customization
     pub head: String,
+    /// Body customization
     pub body: String,
+    /// Feet customization
     pub feet: String,
+    /// Hands customization
     pub hands: String,
 }
 
-#[derive(Debug, Deserialize, Clone, PartialEq)]
-#[serde(rename_all = "PascalCase")]
-pub struct HealthLevel {
-    pub current: u64,
-    pub maximum: u64,
-}
-
-#[derive(Debug, Deserialize, Clone, PartialEq)]
-#[serde(rename_all = "PascalCase")]
-pub struct BodyParts {
-    pub head: Head,
-    pub chest: Chest,
-    pub stomach: Stomach,
-    pub left_arm: LeftArm,
-    pub right_arm: RightArm,
-    pub left_leg: LeftLeg,
-    pub right_leg: RightLeg,
-}
-
-#[derive(Debug, Deserialize, Clone, PartialEq)]
-#[serde(rename_all = "PascalCase")]
-pub struct Head {
-    pub health: HealthLevel,
-}
-
-#[derive(Debug, Deserialize, Clone, PartialEq)]
-#[serde(rename_all = "PascalCase")]
-pub struct Chest {
-    pub health: HealthLevel,
-}
-
-#[derive(Debug, Deserialize, Clone, PartialEq)]
-#[serde(rename_all = "PascalCase")]
-pub struct Stomach {
-    pub health: HealthLevel,
-}
-
-#[derive(Debug, Deserialize, Clone, PartialEq)]
-#[serde(rename_all = "PascalCase")]
-pub struct LeftArm {
-    pub health: HealthLevel,
-}
-
-#[derive(Debug, Deserialize, Clone, PartialEq)]
-#[serde(rename_all = "PascalCase")]
-pub struct RightArm {
-    pub health: HealthLevel,
-}
-
-#[derive(Debug, Deserialize, Clone, PartialEq)]
-#[serde(rename_all = "PascalCase")]
-pub struct LeftLeg {
-    pub health: HealthLevel,
-}
-
-#[derive(Debug, Deserialize, Clone, PartialEq)]
-#[serde(rename_all = "PascalCase")]
-pub struct RightLeg {
-    pub health: HealthLevel,
-}
-
+/// Player health
 #[derive(Debug, Deserialize, Clone, PartialEq)]
 #[serde(rename_all = "PascalCase")]
 pub struct Health {
+    /// Hydration level
     pub hydration: HealthLevel,
+    /// Energy level
     pub energy: HealthLevel,
+    /// Body parts health
     pub body_parts: BodyParts,
+    /// ?
     pub update_time: u64,
 }
 
+/// Health level
 #[derive(Debug, Deserialize, Clone, PartialEq)]
 #[serde(rename_all = "PascalCase")]
-pub struct UpdMedkit {
-    pub hp_resource: u64,
+pub struct HealthLevel {
+    /// Current health
+    pub current: u64,
+    /// Maximum health
+    pub maximum: u64,
 }
 
+/// Body health
 #[derive(Debug, Deserialize, Clone, PartialEq)]
 #[serde(rename_all = "PascalCase")]
-pub struct UpdRepairable {
-    pub max_durability: f64,
-    pub durability: f64,
+pub struct BodyParts {
+    /// Head health
+    pub head: Head,
+    /// Chest health
+    pub chest: Chest,
+    /// Stomach health
+    pub stomach: Stomach,
+    /// Left arm health
+    pub left_arm: LeftArm,
+    /// Right arm health
+    pub right_arm: RightArm,
+    /// Left leg health
+    pub left_leg: LeftLeg,
+    /// Right arm health
+    pub right_leg: RightLeg,
 }
 
+/// Head health
 #[derive(Debug, Deserialize, Clone, PartialEq)]
 #[serde(rename_all = "PascalCase")]
-pub struct UpdLight {
-    pub is_active: bool,
-    pub selected_mode: u64,
+pub struct Head {
+    /// Health
+    pub health: HealthLevel,
 }
 
+/// Chest health
 #[derive(Debug, Deserialize, Clone, PartialEq)]
-#[serde(rename_all = "camelCase")]
-pub struct Location {
-    pub x: u64,
-    pub y: u64,
-    pub r: u64,
-    pub is_searched: Option<bool>,
+#[serde(rename_all = "PascalCase")]
+pub struct Chest {
+    /// Health
+    pub health: HealthLevel,
 }
 
+/// Stomach health
+#[derive(Debug, Deserialize, Clone, PartialEq)]
+#[serde(rename_all = "PascalCase")]
+pub struct Stomach {
+    /// Health
+    pub health: HealthLevel,
+}
+
+/// Left arm health
+#[derive(Debug, Deserialize, Clone, PartialEq)]
+#[serde(rename_all = "PascalCase")]
+pub struct LeftArm {
+    /// Health
+    pub health: HealthLevel,
+}
+
+/// Right arm health
+#[derive(Debug, Deserialize, Clone, PartialEq)]
+#[serde(rename_all = "PascalCase")]
+pub struct RightArm {
+    /// Health
+    pub health: HealthLevel,
+}
+
+/// Left leg health
+#[derive(Debug, Deserialize, Clone, PartialEq)]
+#[serde(rename_all = "PascalCase")]
+pub struct LeftLeg {
+    /// Health
+    pub health: HealthLevel,
+}
+
+/// Right leg health
+#[derive(Debug, Deserialize, Clone, PartialEq)]
+#[serde(rename_all = "PascalCase")]
+pub struct RightLeg {
+    /// Health
+    pub health: HealthLevel,
+}
+
+/// Profile inventory
 #[derive(Debug, Deserialize, Clone, PartialEq)]
 #[serde(rename_all = "camelCase")]
 pub struct Inventory {
+    /// Items
     pub items: Vec<Item>,
+    /// Equipment
     pub equipment: String,
+    /// Stash
     pub stash: Option<String>,
+    /// ?
     pub quest_raid_items: String,
+    /// ?
     pub quest_stash_items: String,
+    // XXX: Find types for these
     // first_panel: {},
 }
 
+/// Player skills
 #[derive(Debug, Deserialize, Clone, PartialEq)]
 #[serde(rename_all = "PascalCase")]
 pub struct Skills {
+    /// Common skills
     pub common: Vec<CommonSkill>,
+    /// Master skills
     pub mastering: Vec<MasteringSkill>,
+    /// Skill points
     pub points: f64,
 }
 
+/// Common skill
 #[derive(Debug, Deserialize, Clone, PartialEq)]
 #[serde(rename_all = "PascalCase")]
 pub struct CommonSkill {
+    /// Skill ID
     pub id: String,
+    /// Skill progress
     pub progress: f64,
+    /// Points earned during session
     pub points_earned_during_session: f64,
+    /// ?
     pub last_access: i64,
 }
 
+/// Master skill
 #[derive(Debug, Deserialize, Clone, PartialEq)]
 #[serde(rename_all = "PascalCase")]
 pub struct MasteringSkill {
+    /// Skill ID
     pub id: String,
+    /// Skill progress
     pub progress: u64,
 }
 
+/// Player statistics
 #[derive(Debug, Deserialize, Clone, PartialEq)]
 #[serde(rename_all = "PascalCase")]
 pub struct Stats {
+    /// Session stats counters
     pub session_counters: SessionCounters,
+    /// Overall stats counters
     pub overall_counters: OverallCounters,
+    /// Session experience multiplier?
     pub session_experience_mult: f64,
+    /// Experience bonus multiplier?
     pub experience_bonus_mult: u64,
+    /// Total session experience
     pub total_session_experience: u64,
+    /// Last session timestamp
     pub last_session_date: u64,
+    /// ?
     pub aggressor: Option<Aggressor>,
+    /// Total game time
     pub total_in_game_time: u64,
+    /// Survivor class
     pub survivor_class: String,
-    // dropped_items: [],
+    // XXX: Find types for these
+    // dropped_items: [], Vec<serde_json::Value>
     // found_in_raid_items: [],
     // victims: [],
     // carried_quest_items: []
 }
 
+/// Session stats counter
 #[derive(Debug, Deserialize, Clone, PartialEq)]
 #[serde(rename_all = "PascalCase")]
 pub struct SessionCounters {
+    /// Statistics
     pub items: Vec<SessionItem>,
 }
 
+/// Overall stats counter
 #[derive(Debug, Deserialize, Clone, PartialEq)]
 #[serde(rename_all = "PascalCase")]
 pub struct OverallCounters {
+    /// Statistics
     pub items: Vec<SessionItem>,
 }
 
+/// Statistics
 #[derive(Debug, Deserialize, Clone, PartialEq)]
 #[serde(rename_all = "PascalCase")]
 pub struct SessionItem {
+    /// Statistic key
     pub key: Vec<String>,
+    /// Statistic value
     pub value: u64,
 }
 
+/// ?
 #[derive(Debug, Deserialize, Clone, PartialEq)]
 #[serde(rename_all = "PascalCase")]
 pub struct Aggressor {
+    /// Aggressor name
     pub name: String,
+    /// Aggressor side
     pub side: Side,
+    /// Aggressor body part
     pub body_part: String,
+    /// Aggressor head segment
     pub head_segment: Option<String>,
+    /// Aggressor weapon name
     pub weapon_name: String,
+    /// Aggressor category
     pub category: String,
 }
 
+/// ?
 #[derive(Debug, Deserialize, Clone, PartialEq)]
 #[serde(rename_all = "PascalCase")]
 pub struct ConditionCounters {
+    /// Counters
     pub counters: Vec<ConditionCounter>,
 }
 
+/// ?
 #[derive(Debug, Deserialize, Clone, PartialEq)]
 #[serde(rename_all = "camelCase")]
 pub struct ConditionCounter {
+    /// Counter ID
     pub id: String,
+    /// Counter value
     pub value: u64,
 }
 
+/// ?
 #[derive(Debug, Deserialize, Clone, PartialEq)]
 #[serde(rename_all = "camelCase")]
 pub struct BackendCounter {
+    /// Counter ID
     pub id: String,
+    /// ?
     pub qid: String,
+    /// Counter value
     pub value: u64,
 }
 
+/// Bonus?
 #[derive(Debug, Deserialize, Clone, PartialEq)]
 #[serde(rename_all = "camelCase")]
 pub struct Bonus {
+    /// ?
     #[serde(rename = "type")]
     pub bonus_type: String,
+    /// ?
     pub template_id: Option<String>,
+    /// ?
     pub value: Option<i64>,
+    /// ?
     pub passive: Option<bool>,
+    /// ?
     pub visible: Option<bool>,
+    /// ?
     pub production: Option<bool>,
+    /// ?
     pub filter: Option<Vec<String>>,
+    /// ?
     pub id: Option<String>,
+    /// ?
     pub icon: Option<String>,
 }
 
+/// Quest
 #[derive(Debug, Deserialize, Clone, PartialEq)]
 #[serde(rename_all = "camelCase")]
 pub struct Quest {
+    /// Quest ID
     #[serde(rename = "qid")]
     pub id: String,
+    /// Quest start time
     pub start_time: u64,
+    /// Quest status
     pub status: u64,
+    /// Quest status timers
     pub status_timers: HashMap<String, u64>,
 }
 
+/// Flea market profile
 #[derive(Debug, Deserialize, Clone, PartialEq)]
 #[serde(rename_all = "camelCase")]
 pub struct Ragfair {
+    /// Market rating
     pub rating: f64,
+    /// Market rating is growing
     pub is_rating_growing: bool,
+    // Active offers (items for sale)
+    // XXX: Find types for these
     // offers: []
 }
 
@@ -359,18 +482,26 @@ struct SelectResponse {
     data: Option<SelectResult>,
 }
 
+/// Profile select result
 #[derive(Debug, Deserialize, Clone, PartialEq)]
 #[serde(rename_all = "camelCase")]
 pub struct SelectResult {
+    /// Profile status
     pub status: String,
+    /// Profile notifier
     pub notifier: Notifier,
+    /// Profile notifier server
     pub notifier_server: String,
 }
 
+/// Profile notifier
 #[derive(Debug, Deserialize, Clone, PartialEq)]
 pub struct Notifier {
+    /// Notifier server
     pub server: String,
+    /// Notifier channel ID
     pub channel_id: String,
+    /// Notifier URL
     pub url: String,
 }
 
